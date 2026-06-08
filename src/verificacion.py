e3a92a840de590f5b1f396acb13e3eb7ea6af48b
omuller07 <omuller@udesa.edu.ar>
2026-06-08 13:37:36 -0300

    docstrings verificacion

--- src/verificacion.py
+++ src/verificacion.py
@@ -1,4 +1,19 @@
 def verificar_disponibilidad(df_disponibilidad):
+    '''
+    Verifica si el usuario tiene horas disponibles cada dia.
+
+    Parameters
+    ----------
+    df_disponibilidad : DataFrame
+        Contiene los datos de la disponibilidad semanal del usuario
+
+    Returns
+    -------
+    bool
+        False si ese dia no tiene horas disponibles.
+        True si hay horas disponible.
+
+    '''
 
     dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
 
@@ -17,6 +32,22 @@ def verificar_disponibilidad(df_disponibilidad):
 
 
 def verificar_fechas_materias(df_materias):
+    '''
+    Verifica que la fecha de examen de cada materia sea posterior
+    a su fecha de inicio.
+
+    Parameters
+    ----------
+    df_materias : DataFrame
+        Contiene los datos de cada materia (temas, fecha de examen, fecha de inicio).
+
+    Returns
+    -------
+    bool
+        False si la fecha del examen es antes de la fecha de inicio.
+        True si la fecha de examen es despues de la fecha de inicio.
+
+    '''
 
     for indice, fila in df_materias.iterrows():
 
@@ -28,6 +59,21 @@ def verificar_fechas_materias(df_materias):
 
 
 def verificar_nombres_materias(df_materias):
+    '''
+    Verifica que cada materia tenga un nombre asociado. 
+
+    Parameters
+    ----------
+    df_materias : DataFrame
+        Contiene los datos de cada materia (temas, fecha de examen, fecha de inicio).
+
+    Returns
+    -------
+    bool
+        True si todas las materias tienen nombre.
+        False si existe al menos una materia con nombre vacío.
+
+    '''
 
     for indice, fila in df_materias.iterrows():
 
@@ -39,6 +85,20 @@ def verificar_nombres_materias(df_materias):
 
 
 def verificar_nombres_temas(df_temas):
+    '''
+    Verfica que cada tema tenga un nombre asociado.
+
+    Parameters
+    ----------
+    df_temas : DataFrame
+        Contiene la informacion de cada tema dentro de cada materia (la dificultad, conocimiento previo, urgencia).
+
+    Returns
+    -------
+    bool
+        False si hay un tema sin nombre.
+        True si todos los temas tienen nombre.
+    '''
 
     for indice, fila in df_temas.iterrows():
 
